241b5b0fb9e4046c46f778584d0ae52a74b5d9e6
georgekrax <georgekraxt@gmail.com>
2021-05-11 22:54:07 +0300

    Remove the `packages/server/build` folder

--- packages/server/build/constants/prefixes.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = Object.freeze({
-    PAYMENT_TARGET_GROUP: "tg_",
-});
--- packages/server/build/constants/status.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.payment = void 0;
-exports.payment = Object.freeze({
-    CREATED: "CREATED",
-    PAID: "PAID",
-    REFUNDED: "REFUNDED",
-});
